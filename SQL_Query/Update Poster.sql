08d33bd635af8ae5daff51af0c0f16fa4eba5966
thanhle1547 <thanhlm72@wru.vn>
2019-10-23 14:32:44 +0700

    Add 1 movie to Table Phim

--- SQL_Query/Update Poster.sql
+++ SQL_Query/Update Poster.sql
@@ -8,3 +8,8 @@ Update Phim
 	Set Poster = BulkColumn
 		From OpenRowSet (Bulk 'D:\Learning\C#\Cinema Management\Movie Posters\avengers-endgame-poster-3d.png', Single_Blob ) as img
 			Where IDPhim = 2
+
+Update Phim 
+	Set Poster = BulkColumn
+		From OpenRowSet (Bulk 'D:\Learning\C#\Cinema Management\Movie Posters\Frozen 2.png', Single_Blob ) as img
+			Where IDPhim = 3
